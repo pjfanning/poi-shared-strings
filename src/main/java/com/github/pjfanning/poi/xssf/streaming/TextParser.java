9a46ca11aae855251007cc8612363cca6e975ced
PJ Fanning <pjfanning@users.noreply.github.com>
2021-10-12 12:22:32 +0100

    Update TextParser.java

--- src/main/java/com/github/pjfanning/poi/xssf/streaming/TextParser.java
+++ src/main/java/com/github/pjfanning/poi/xssf/streaming/TextParser.java
@@ -22,6 +22,56 @@ class TextParser {
 
     private static final XMLEventFactory xef = XMLHelper.newXMLEventFactory();
 
+    /**
+     * Parses a {@code <si>} String Item. Returns just the text and drops the formatting. See <a
+     * href="https://msdn.microsoft.com/en-us/library/documentformat.openxml.spreadsheet.sharedstringitem.aspx">xmlschema
+     * type {@code CT_Rst}</a>.
+     */
+    static String parseCT_Rst(XMLEventReader xmlEventReader) throws XMLStreamException {
+        // Precondition: pointing to <si> or <text>;  Post condition: pointing to </si> or </text>
+        StringBuilder buf = new StringBuilder();
+        XMLEvent xmlEvent;
+        while((xmlEvent = xmlEventReader.nextTag()).isStartElement()) {
+            switch(xmlEvent.asStartElement().getName().getLocalPart()) {
+                case "t": // Text
+                    buf.append(xmlEventReader.getElementText());
+                    break;
+                case "r": // Rich Text Run
+                    parseCT_RElt(xmlEventReader, buf);
+                    break;
+                case "rPh": // Phonetic Run
+                case "phoneticPr": // Phonetic Properties
+                    skipElement(xmlEventReader);
+                    break;
+                default:
+                    throw new IllegalArgumentException("Unexpected start element: " + xmlEvent.asStartElement().getName().getLocalPart());
+            }
+        }
+        return buf.toString();
+    }
+
+    /**
+     * Parses a {@code <r>} Rich Text Run. Returns just the text and drops the formatting. See <a
+     * href="https://msdn.microsoft.com/en-us/library/documentformat.openxml.spreadsheet.run.aspx">xmlschema
+     * type {@code CT_RElt}</a>.
+     */
+    static void parseCT_RElt(XMLEventReader xmlEventReader, StringBuilder buf) throws XMLStreamException {
+        // Precondition: pointing to <r>;  Post condition: pointing to </r>
+        XMLEvent xmlEvent;
+        while((xmlEvent = xmlEventReader.nextTag()).isStartElement()) {
+            switch(xmlEvent.asStartElement().getName().getLocalPart()) {
+                case "t": // Text
+                    buf.append(xmlEventReader.getElementText());
+                    break;
+                case "rPr": // Run Properties
+                    skipElement(xmlEventReader);
+                    break;
+                default:
+                    throw new IllegalArgumentException("Unexpected start element: " + xmlEvent.asStartElement().getName().getLocalPart());
+            }
+        }
+    }
+
     static String getXMLText(XMLEventReader xmlEventReader, QName tag, List<String> wrappingTags) throws IOException, XMLStreamException {
         try (StringWriter sw = new StringWriter()) {
             XMLEventWriter xew = XMLHelper.newXMLOutputFactory().createXMLEventWriter(sw);
@@ -65,5 +115,12 @@ class TextParser {
             }
         }
         return event;
+    }
+
+    private static void skipElement(XMLEventReader xmlEventReader) throws XMLStreamException {
+        // Precondition: pointing to start element;  Post condition: pointing to end element
+        while(xmlEventReader.nextTag().isStartElement()) {
+            skipElement(xmlEventReader); // recursively skip over child
+        }
     }
 }
