4a3c6e03b6d9a567fc16384f38ec5108aaac087e
PJ Fanning <pjfanning@users.noreply.github.com>
2021-10-13 01:15:51 +0100

    Create TestSXSSFWorkbookWithTempCommentsTable.java

---
+++ src/test/java/com/github/pjfanning/poi/xssf/streaming/TestSXSSFWorkbookWithTempCommentsTable.java
@@ -0,0 +1,27 @@
+package com.github.pjfanning.poi.xssf.streaming;
+
+import org.apache.poi.ss.usermodel.ClientAnchor;
+import org.apache.poi.ss.usermodel.Comment;
+import org.apache.poi.xssf.streaming.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.junit.Test;
+
+public class TestSXSSFWorkbookWithTempCommentsTable {
+    @Test
+    public void testComments() throws Exception {
+        SXSSFFactory factory = new SXSSFFactory();
+        factory.enableTempFileComments(true);
+        SXSSFWorkbook wb = new SXSSFWorkbook(new XSSFWorkbook(factory),
+                SXSSFWorkbook.DEFAULT_WINDOW_SIZE, false, false);
+        SXSSFSheet sheet = wb.createSheet("testSheet");
+        ClientAnchor anchor = wb.getCreationHelper().createClientAnchor();
+        SXSSFRow row = sheet.createRow(0);
+        SXSSFCell cell = row.createCell(0);
+        cell.setCellValue("cell1");
+        SXSSFDrawing drawing = sheet.createDrawingPatriarch();
+        Comment comment = drawing.createCellComment(anchor);
+        comment.setString(wb.getCreationHelper().createRichTextString("comment1"));
+        cell.setCellComment(comment);
+        wb.close();
+    }
+}
