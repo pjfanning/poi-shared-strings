6ab3d295205fae19f8fbc3b27c1306dc5375e0e5
PJ Fanning <pjfanning@users.noreply.github.com>
2021-11-20 13:23:25 +0100

    Update TempFileCommentsTable.java

--- src/main/java/com/github/pjfanning/poi/xssf/streaming/TempFileCommentsTable.java
+++ src/main/java/com/github/pjfanning/poi/xssf/streaming/TempFileCommentsTable.java
@@ -106,9 +106,9 @@ public class TempFileCommentsTable extends POIXMLDocumentPart implements Comment
     @Override
     protected void commit() throws IOException {
         PackagePart part = getPackagePart();
-        OutputStream out = part.getOutputStream();
-        writeTo(out);
-        out.close();
+        try (OutputStream out = part.getOutputStream()) {
+            writeTo(out);
+        }
     }
 
     /**
